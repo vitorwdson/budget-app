60dd20734818c5129e4df12fedbcd6c2cbe0680a
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-20 15:53:40 -0400

    Setup for deploying on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: yarn build:server && node packages/server/dist/main.js
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = '8000';
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(4000);
+  await app.listen(parseInt(port));
 }
 bootstrap();
