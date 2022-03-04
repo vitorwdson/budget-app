5f2047ed20b54e405bc34ffd1e3b4da66d783fd8
Vitor Wdson <vitor.wdson2@gmail.com>
2022-03-03 20:31:25 -0400

    Disabled GraphQL Playground in production

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BudgetsModule } from './budgets/budgets.module';
 import { ExpensesModule } from './expenses/expenses.module';
@@ -19,13 +17,14 @@ import { ExpensesModule } from './expenses/expenses.module';
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       context: ({ res }) => res,
+      playground: process.env.DEV_MODE == 'true',
       cors: {
         origin: process.env.FRONTEND_URL,
         credentials: true,
       },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
